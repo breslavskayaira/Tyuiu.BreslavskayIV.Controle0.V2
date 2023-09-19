d6ef744546a1c9174924166f2e4cf1439d5062a4
BreslavskayaIrina <144372200+breslavskayaira@users.noreply.github.com>
2023-09-19 12:59:21 +0500

    Add files via upload

---
+++ Tyuiu.BreslavskayaIV.Controle0.V2.Task1.Lib/Tyuiu.BreslavskayaIV.Controle0.V2.Task1.Lib.cpp
@@ -0,0 +1,27 @@
+﻿// Tyuiu.BreslavskayaIV.Controle0.V2.Task1.Lib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "math.h"
+#include "../Tyuiu.CoursControl_0/Tyuiu.CoursControl_0.cpp"
+
+// TODO: This is an example of a library function
+class Service0 : public ISprint0Task8V1
+{
+    virtual float Chislo(float x, float z) override
+    {
+        float y;
+        float a;
+        a = pow(x, 4);
+        float b;
+        b = sqrt(234.0 + z);
+        float c;
+        c = (12 * x + a) / (x * x + b);
+        y = sqrt(c);
+        return y;
+
+
+
+    }
+};
---
+++ Tyuiu.BreslavskayaIV.Controle0.V2.Task1.Test/Tyuiu.BreslavskayaIV.Controle0.V2.Task1.Test.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tyuiu.BreslavskayaIV.Controle0.V2.Task1.Lib/Tyuiu.BreslavskayaIV.Controle0.V2.Task1.Lib.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TyuiuBreslavskayaIVControle0V2Task1Test
+{
+	TEST_CLASS(TyuiuBreslavskayaIVControle0V2Task1Test)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			ISprint0Task8V1* date = new Service0();
+			float x = 1;
+			float z = 1;
+			float y;
+			y = date->Chislo(x, z);
+			int f;
+			f = int(y);
+			Assert::AreEqual(0, f);
+		}
+	};
+}
